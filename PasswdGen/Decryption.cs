cbb70ef4279c5312b3bee5d7f2fb1dbbf8beb0fa
Janso123 <mr.k0l3k.5@gmail.com>
2017-05-22 22:24:09 +0200

    Some corrections
    change from file stream to memory stream in sam places
    some code clerance

---
+++ PasswdGen/Decryption.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PasswdGen
+{
+    class Decryption
+    {
+        private string srtOut;
+        public string File(string inputFile, string skey)
+        {
+            try
+            {
+                using (RijndaelManaged aes = new RijndaelManaged())
+                {
+                    byte[] key = ASCIIEncoding.UTF8.GetBytes(skey);
+
+                    byte[] IV = ASCIIEncoding.UTF8.GetBytes(skey);
+
+                    using (FileStream fsCrypt = new FileStream(inputFile, FileMode.Open))
+                    {
+                        using (MemoryStream msOut = new MemoryStream())
+                        {
+                            using (ICryptoTransform decryptor = aes.CreateDecryptor(key, IV))
+                            {
+                                using (CryptoStream cs = new CryptoStream(fsCrypt, decryptor, CryptoStreamMode.Read))
+                                {
+                                    int data;
+                                    while ((data = cs.ReadByte()) != -1)
+                                    {
+                                        msOut.WriteByte((byte)data);
+                                    }
+                                    msOut.Position = 0;
+                                    byte[] t = msOut.ToArray();
+                                    srtOut = Encoding.UTF8.GetString(t);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex}");
+            }
+            return srtOut;
+        }
+    }
+}
--- PasswdGen/Program.cs
+++ PasswdGen/Program.cs
@@ -1,20 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Security.Cryptography;
-using System.Text;
 
 namespace PasswdGen
 
 {
     class Program
     {
-        private static string tempPassFile = "./passwd.temp";
+        private static string tempPassString;
         private static string passFile = "./passwd.passwd";
         private static string keyToFile;
-        private static bool _isPassToShow = false;
-        static public List<char> asciiCharAray = new List<char>();
+        static public List<char> asciiCharAray = new List<char>("!@#$%^&*()abcdefghijklmnopqrstuvwxyz1234567890?;:ABCDEFGHIJKLMNOPQRSTUVWXYZ^&".ToCharArray());
         static public List<char> passwLetters = new List<char>();
+        private static Encryption Encrypt = new Encryption();
+        private static Decryption Decrypt = new Decryption();
+
         static void Main(string[] args)
         {
             while (Menu()) { };
@@ -54,43 +54,24 @@ namespace PasswdGen
         static public void NewPasswdDialog()
         {
             TextDialog Dialog = new TextDialog();
-            keyToFile = Dialog.keyInput();
-            asciiAray();
-            passwdLetterConv(Dialog.passwdInput());
 
+            keyToFile = Dialog.keyInput();
+            passwLetters = new List<char>(Dialog.passwdInput().ToCharArray());
             string ret = "Passwd: " + passEncryption() + " Palce: " + Dialog.placeInput() + "\r\n";
             if (File.Exists(passFile))
             {
-                DecryptFile(passFile, tempPassFile, keyToFile);
+                tempPassString = Decrypt.File(passFile, keyToFile);
             }
-            File.AppendAllText(tempPassFile, ret);
-            EncryptFile(tempPassFile, passFile, keyToFile);
-            File.Delete(tempPassFile);
+            tempPassString = tempPassString + ret;
+            
+            Encrypt.File(tempPassString, passFile, keyToFile);
         }
         static public void ReadPasswdFile()
         {
             TextDialog Dialog = new TextDialog();
+            Decryption EnDe = new Decryption();
             keyToFile = Dialog.keyInput();
-            _isPassToShow = true;
-            DecryptFile(passFile, tempPassFile, keyToFile);
-        }
-        static public void asciiAray()
-        {
-            asciiCharAray = new List<char>();
-            char[] c = "!@#$%^&*()abcdefghijklmnopqrstuvwxyz1234567890?;:ABCDEFGHIJKLMNOPQRSTUVWXYZ^&".ToCharArray();
-            for (int i = 0; i < c.Length; i++)
-            {
-                asciiCharAray.Add(c[i]);
-
-            }
-        }
-        static public void passwdLetterConv(string passwd)
-        {
-            passwLetters = new List<char>();
-            for (int i = 0; i < passwd.Length; i++)
-            {
-                passwLetters.Add(passwd[i]);
-            }
+            Console.WriteLine(Decrypt.File(passFile, keyToFile));
         }
 
         static public string passEncryption()
@@ -112,92 +93,6 @@ namespace PasswdGen
 
             return encPasswd;
         }
-        private static void EncryptFile(string inputFile, string outputFile, string skey)
-        {
-            try
-            {
-                using (RijndaelManaged aes = new RijndaelManaged())
-                {
-                    byte[] key = ASCIIEncoding.UTF8.GetBytes(skey);
-
-                    /* This is for demostrating purposes only. 
-                     * Ideally you will want the IV key to be different from your key and you should always generate a new one for each encryption in other to achieve maximum security*/
-                    byte[] IV = ASCIIEncoding.UTF8.GetBytes(skey);
-
-                    using (FileStream fsCrypt = new FileStream(outputFile, FileMode.Create))
-                    {
-                        using (ICryptoTransform encryptor = aes.CreateEncryptor(key, IV))
-                        {
-                            using (CryptoStream cs = new CryptoStream(fsCrypt, encryptor, CryptoStreamMode.Write))
-                            {
-                                using (FileStream fsIn = new FileStream(inputFile, FileMode.Open))
-                                {
-                                    int data;
-                                    while ((data = fsIn.ReadByte()) != -1)
-                                    {
-                                        cs.WriteByte((byte)data);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Błąd");
-            }
-        }
-        private static void DecryptFile(string inputFile, string outputFile, string skey)
-        {
-            try
-            {
-                using (RijndaelManaged aes = new RijndaelManaged())
-                {
-                    byte[] key = ASCIIEncoding.UTF8.GetBytes(skey);
-
-                    byte[] IV = ASCIIEncoding.UTF8.GetBytes(skey);
-
-                    using (FileStream fsCrypt = new FileStream(inputFile, FileMode.Open))
-                    {
-                        using (FileStream fsOut = new FileStream(outputFile, FileMode.Create))
-                        {
-                            using (ICryptoTransform decryptor = aes.CreateDecryptor(key, IV))
-                            {
-                                using (CryptoStream cs = new CryptoStream(fsCrypt, decryptor, CryptoStreamMode.Read))
-                                {
-                                    int data;
-                                    List<byte> output = new List<byte>();
-                                    while ((data = cs.ReadByte()) != -1)
-                                    {
-                                        if (_isPassToShow)
-                                        {
-                                            output.Add((byte)data);
-
-                                        }
-                                        else
-                                            fsOut.WriteByte((byte)data);
-                                    }
-                                    if (_isPassToShow)
-                                    {
-                                        byte[] ret = output.ToArray();
-                                        Console.WriteLine(Encoding.UTF8.GetString(ret));
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    if (File.Exists(tempPassFile) && _isPassToShow)
-                    {
-                        File.Delete(tempPassFile);
-                        _isPassToShow = false;
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
-        }
+        
     }
 }
